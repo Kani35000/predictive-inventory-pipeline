f32218689e544cd6a4637eb296d626c1b0f88363
Kani35000 <kanidayeokorji@gmail.com>
2026-05-09 10:35:23 -0500

    Add demand 7day/30day moving average divergence analysis

--- 01_database/demand_forecast.sql
+++ 01_database/demand_forecast.sql
@@ -34,3 +34,55 @@ ORDER BY
     w.warehouse_name,
     p.product_name,
     t.transaction_date;
+
+
+
+-- ============================================
+-- MOORE INSIGHTS: DIVERGENCE ANALYSIS
+-- Comparason between 7 Day and 30 Day Simple Moving Average Divergence greater than 10units sold
+-- ============================================
+
+WIth forecast AS(
+SELECT
+    w.warehouse_name,
+    p.product_name,
+    p.category,
+    t.transaction_date,
+    t.units_sold,
+    ROUND(
+        AVG(t.units_sold) OVER (
+            PARTITION BY t.warehouse_id, t.product_id
+            ORDER BY t.transaction_date
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        )::numeric, 2) AS sma_7_day,
+    ROUND(
+        AVG(t.units_sold) OVER (
+            PARTITION BY t.warehouse_id, t.product_id
+            ORDER BY t.transaction_date
+            ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+        )::numeric, 2) AS sma_30_day
+FROM retail.inventory_transactions t
+JOIN retail.products p 
+    ON t.product_id = p.product_id
+JOIN retail.warehouses w 
+    ON t.warehouse_id = w.warehouse_id
+WHERE t.transaction_date >= (
+    SELECT MAX(transaction_date) - INTERVAL '30 days'
+    FROM retail.inventory_transactions
+)
+ORDER BY 
+    w.warehouse_name,
+    p.product_name,
+    t.transaction_date
+)
+SELECT
+    warehouse_name,
+    product_name,
+    transaction_date,
+    sma_7_day,
+    sma_30_day,
+    ROUND(sma_7_day - sma_30_day, 2) 
+        AS demand_trend_signal
+FROM forecast
+WHERE ABS(sma_7_day - sma_30_day) > 10
+ORDER BY ABS(sma_7_day - sma_30_day) DESC
